0641bb184084cb90b045c6f8ab6c8000d70ea77c
Connor Bradley <109628356+cobrastats@users.noreply.github.com>
2025-01-24 09:52:51 -0500

    I spelled "length" wrong...

--- killshot_code.R
+++ killshot_code.R
@@ -22,14 +22,14 @@ pbp = bigballR::get_play_by_play(schedule$Game_ID)
     dplyr::group_by(ID,run) %>% 
     dplyr::summarise(run_team = first(Event_Team),
               def_team = if_else(run_team==first(Home),first(Away),first(Home)),
-              run_legth = sum(Shot_Value),
+              run_length = sum(Shot_Value),
               start_score = if_else(run_team==first(Home),paste0(first(Home_Score)-first(Shot_Value),"-",first(Away_Score)),
                                                                  paste0(first(Away_Score)-first(Shot_Value),"-",first(Home_Score))),
               end_score = if_else(run_team==first(Home),paste0(last(Home_Score),"-",last(Away_Score)),
                                   paste0(last(Away_Score),"-",last(Home_Score))),
               run_start = paste0(first(Half_Status),"H-",first(Time)),
               run_end = paste0(last(Half_Status),"H-",last(Time))) %>% 
-    dplyr::arrange(desc(run_legth)) %>% 
+    dplyr::arrange(desc(run_length)) %>% 
     dplyr::select(-run) %>% 
     dplyr::ungroup()
   
